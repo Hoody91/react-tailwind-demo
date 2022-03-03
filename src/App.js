2ee84db9013ee1a3ce840be2010b0c35b956637c
Riley Hood <riley.hood18@gmail.com>
2022-03-03 21:21:49 +1100

    A basic React page with Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import React from "react";
 
+function clickMe() {
+  alert("Hire Me");
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="min-h-screen bg-blue-300">
+      <div className="flex flex-col justify-center items-center bg-blue-300">
+        <div className="m-10 p-10 bg-green-500 rounded-2xl shadow-2xl ">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-semibold text-center">Tailwind CSS</h1>
+            <p className="p-5 font-light ">Do you like my Tailwind demo?</p>
+            <button
+              onClick={clickMe}
+              className="p-5 bg-white hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              Press Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
 
